2e031107c013d2f0dcf4633b33832d68e1902f02
labuser <labuser@FSE.i0sybmzqu3ludfzj1gfnuw33le.rx.internal.cloudapp.net>
2022-12-05 13:51:40 +0000

    junit testcase

---
+++ Junit5Testing/src/com/java/voterage/testMyDemo.java
@@ -0,0 +1,14 @@
+package com.java.voterage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testMyDemo {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
